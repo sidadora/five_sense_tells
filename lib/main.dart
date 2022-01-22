c57377acbb36e6dd83fff2493edcba1444f155ed
柴田竜之介 <s1dadora.1126801@gmail.com>
2022-01-22 17:59:40 +0900

    りゅうちゃんじゃないよに変更

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'りゅうちゃん',
+              'りゅうちゃんじゃないよ',
             ),
             Text(
               '$_counter',
